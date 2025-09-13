f99d26376ea370c96601343928250b2042aa5c10
YoruAlptraum <yorualptraum@gmail.com>
2025-09-13 17:48:28 -0300

    update hardware config

--- .nixos/hardware-configuration.nix
+++ .nixos/hardware-configuration.nix
@@ -8,23 +8,25 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "usbhid" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/193c1793-9dee-4bf8-9608-91548c7ce503";
+    { device = "/dev/disk/by-uuid/11614dad-f25f-41a3-aa52-b254aad3e720";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/652D-0DBB";
+    { device = "/dev/disk/by-uuid/09B4-DA02";
       fsType = "vfat";
       options = [ "fmask=0077" "dmask=0077" ];
     };
 
-  swapDevices = [ ];
+  swapDevices =
+    [ { device = "/dev/disk/by-uuid/8d035b44-653d-42d9-8c17-eef16ba2e840"; }
+    ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
