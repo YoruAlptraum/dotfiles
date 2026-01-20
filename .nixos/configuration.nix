2e679983d191c6f4c176b96e0c3a3e6187fc8c55
YoruAlptraum <yorualptraum@gmail.com>
2026-01-20 16:41:33 -0300

    add mozc
    added japanese language writing support

--- .nixos/configuration.nix
+++ .nixos/configuration.nix
@@ -21,6 +21,26 @@
   time.timeZone = "America/Sao_Paulo";
 
   i18n = {
+    inputMethod = {
+      enable = true;
+      type = "fcitx5";
+      fcitx5 = {
+        settings.inputMethod = {
+          "Groups/0" = {
+            Name = "Default";
+            "Default Layout" = "us";
+            DefaultIM = "mozc";
+          };
+          "Groups/0/Items/0".Name = "keyboard-jp";
+          "Groups/0/Items/1".Name = "mozc";
+          GroupOrder."0" = "Default";
+        };
+        addons = with pkgs; [
+          fcitx5-mozc
+          fcitx5-gtk
+        ];
+      };
+    };
     supportedLocales = [
       "en_US.UTF-8/UTF-8"
       "zh_TW.UTF-8/UTF-8"
